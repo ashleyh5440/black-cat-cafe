1860208fcad9b1ca7d20fefaa18aff8ccc7a16b6
Ashley <ashleyh5440@gmail.com>
2025-01-07 20:32:03 -0500

    started working on homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,25 @@
+import Container from 'react-bootstrap/Container';
+
+import './style.css';
+
 function Home() {
     return (
-        <div>hi</div>
+        <section id="home-page">
+            <div id="home-img-box">
+                <Container id="tagline">
+                    {/* <p>Good juju in every drop</p> */}
+                    <p>Brewing magic and stories since the first.</p>
+                </Container>
+            </div>
+            <Container>
+                    <div>
+                        <p>At Black Cat Café and Bookstore, every taste of our delicious concoctions tells a story, and every page holds magic. We have been crafting magic and stories since the first word was spoken, honoring our diaspora from around the world. </p>
+                    </div>
+                </Container>
+        </section>
     )
 }
 
 export default Home;
+
+{/* <img src= "/images/inside.png" /> */}
